113de88903e6f0edf9665aecc4b138908389879e
abhiteja29 <54380112+abhiteja29@users.noreply.github.com>
2019-10-14 15:28:18 -0500

    Changes made

--- problem.test.js
+++ problem.test.js
@@ -1,6 +1,6 @@
 QUnit.test('TEST add', assert => {
   assert.equal(speed(10, 5), 2, 'correct output')
-  assert.equal(speed(20, 5), 6, 'correct output')
+  assert.equal(speed(20, 5), 6, 'In-correct output')
   assert.equal(speed(4, 5), 0.8, 'correct output')
   assert.equal(speed(36,6), 6, 'correct output')
   assert.equal(speed(4, 0), 5, 'correct output')
